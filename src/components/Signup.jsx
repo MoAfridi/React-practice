88a52ea22c5f50985640b8efca40addedfd7424b
MoAfridi <afridihossain220@gmail.com>
2026-04-10 08:28:49 +0600

    use formik

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,25 +1,55 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
+import {useFormik} from 'formik';
 
 export default function Signup() {
-    const [name, setName] = useState("")
+    const formik = useFormik({
+        initialValues : {
+            name : "",
+            email : "",
+            password : ""
+        },
+        onSubmit : (values , {resetForm}) => {
+            console.log(values);
+            resetForm({values : ""});
+        }
+    });
 
-    const onHandleClick = (e) => {
-        setName(e.target.value)
-    };
-
-    const handleSubmit = (e) => {
-        e.preventDefault()
-        console.log(name)
-    };
-
-    return (
+  return (
+    <div>
+      <h2>Sign Up</h2>
+      <form onSubmit={formik.handleSubmit}>
         <div>
-            <h2>Sign Up</h2>
-            <form onSubmit={handleSubmit}>
-                <label htmlFor="name">Name : </label>
-                <input type="text" id='name' name='name' onChange={onHandleClick} value={name}/>
-                <button type='submit'>Sign up</button>
-            </form>
+          <label htmlFor="name">Name : </label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            onChange={formik.handleChange}
+            value={formik.values.name}
+          />
         </div>
-    )
+        <div>
+          <label htmlFor="email">Email : </label>
+          <input
+            type="text"
+            id="email"
+            name="email"
+            onChange={formik.handleChange}
+            value={formik.values.email}
+          />
+        </div>
+        <div>
+          <label htmlFor="password,">Password : </label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            onChange={formik.handleChange}
+            value={formik.values.password}
+          />
+        </div>
+        <button type="submit">Sign up</button>
+      </form>
+    </div>
+  );
 }
