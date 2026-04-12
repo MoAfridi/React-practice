50d4290a552f81edfbb8bb9250892601966e48ab
MoAfridi <afridihossain220@gmail.com>
2026-04-13 00:01:50 +0600

    use formik

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import './App.css'
 import Signup from './components/Signup'
+import Toggle from './Toggle/Toggle'
 
 function App() {
   return (
     <>
-      <Signup />
+      < Toggle />
     </>
   )
 }
---
+++ src/Toggle/Toggle.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Toggle() {
+  return (
+    <div style={{color: "red"}}> 
+      <p>lorem
+      </p>
+    </div>
+  )
+}
