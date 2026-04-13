d4422837772d073311ebbe7885c7eb9449c37b44
MoAfridi <afridihossain220@gmail.com>
2026-04-13 22:58:16 +0600

    use toggole

--- src/Toggle/Toggle.jsx
+++ src/Toggle/Toggle.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 export default function Toggle() {
+  const [isVisible, setIsVisible] = useState(true);
+
+  const handleShowHide = () => {
+    setIsVisible(!isVisible);
+  };
   return (
-    <div style={{color: "red"}}> 
-      <p>lorem
-      </p>
+    <div style={{color: "red",backgroundColor: "black"}}> 
+      {isVisible && <p>Rimi ki amk biye korbe</p>}
+      <div style={{textAlign : "center", paddingTop : "50px", gap : "20px"}}>
+      <button onClick={handleShowHide} value={isVisible}>
+        {isVisible ? "Hide" : "Show"}
+      </button>
+      </div>
     </div>
   )
 }
